b6c65fefe57be379fc30959ac7eed775fef7f7b0
Jaliya Udagedara <jaliya.udagedara@hotmail.com>
2021-12-08 14:52:55 +1300

    Code cleanup

--- src/ContainerAppsDemo.Web.Blazor/Services/CustomerService.cs
+++ src/ContainerAppsDemo.Web.Blazor/Services/CustomerService.cs
@@ -6,28 +6,20 @@ namespace ContainerAppsDemo.Web.Blazor.Services
     public class CustomerService : ICustomerService
     {
         private readonly DaprClient _daprClient;
+        private readonly string _serviceId;
 
-        public CustomerService(DaprClient daprClient)
+        public CustomerService(DaprClient daprClient, IConfiguration configuration)
         {
             _daprClient = daprClient;
+            _serviceId = configuration.GetValue<string>("Services:Customers:ServiceId");
         }
 
         public async Task<IEnumerable<Customer>> GetCustomers(CancellationToken cancellationToken = default)
         {
-            IEnumerable<Customer>? customers = null;
-            try
-            {
-                customers = await _daprClient.InvokeMethodAsync<List<Customer>>(HttpMethod.Get,
-                    "customers-api",
-                    "customers",
-                    cancellationToken);
-            }
-            catch (Exception ex)
-            {
-
-            }
-
-            return customers;
+            return await _daprClient.InvokeMethodAsync<List<Customer>>(HttpMethod.Get,
+                _serviceId,
+                "customers",
+                cancellationToken);
         }
     }
 }
--- src/ContainerAppsDemo.Web.Blazor/Services/OrderService.cs
+++ src/ContainerAppsDemo.Web.Blazor/Services/OrderService.cs
@@ -6,20 +6,20 @@ namespace ContainerAppsDemo.Web.Blazor.Services
     public class OrderService : IOrderService
     {
         private readonly DaprClient _daprClient;
+        private readonly string _serviceId;
 
-        public OrderService(DaprClient daprClient)
+        public OrderService(DaprClient daprClient, IConfiguration configuration)
         {
             _daprClient = daprClient;
+            _serviceId = configuration.GetValue<string>("Services:Orders:ServiceId");
         }
 
         public async Task<IEnumerable<Order>> GetOrders(CancellationToken cancellationToken = default)
         {
-            IEnumerable<Order>? orders = await _daprClient.InvokeMethodAsync<IEnumerable<Order>>(HttpMethod.Get,
-                "orders-api",
+            return await _daprClient.InvokeMethodAsync<IEnumerable<Order>>(HttpMethod.Get,
+                _serviceId,
                 "orders",
                 cancellationToken);
-
-            return orders;
         }
     }
 }
